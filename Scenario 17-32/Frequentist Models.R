ee0460665d5475c76704ae097ed44e8a15f456fb
Theodoros Evrenoglou <43581879+TEvrenoglou@users.noreply.github.com>
2022-01-26 11:29:58 +0100

    Update Frequentist Models.R

--- Scenario 17-32/Frequentist Models.R
+++ Scenario 17-32/Frequentist Models.R
@@ -2,7 +2,7 @@ library(parallel)
 library(beepr)
 library(netmeta)
 library(brglm)
-library(lme4)
+
 
 
 
@@ -574,7 +574,6 @@ mean(length_profile)
 rm(FL_F)
 rm(l8)
 rm(X2)
-#### Binomial-Normal model
 
 no_cores2 <- detectCores()#-3
 cl2 <- makeCluster(no_cores2)
@@ -584,28 +583,12 @@ X3=list()
 for (i in 1:N.sim){ X3[[i]]=list("data"=data2[[i]],"logOR"=logOR[[i]]) }
 
 ################################################################
-###### Binomial-Normal ###########
+###### Common effect logistic regression with standard likelihood ###########
 BN=function(X)
 {
-  biasBN<-c()
-  coverageBN<-c()
-  mseBN=c()
-  se_BN=c()
-  length_BN=c()
-  lower_BN=c()
-  upper_BN=c()
-  estimate_BN=c()
-  count_fail_fixed=c()
+
   
-  biasBN_random<-c()
-  coverageBN_random<-c()
-  mseBN_random=c()
-  se_BN_random=c()
-  length_BN_random=c()
-  lower_BN_random=c()
-  upper_BN_random=c()
-  estimate_BN_random=c()
-  count_fail_random=c()
+
   
   biasBN_glm<-c()
   coverageBN_glm<-c()
@@ -616,100 +599,12 @@ BN=function(X)
   upper_glm=c()
   estimate_glm=c()
   
-  BN1=glmer(cbind(events,n-events)~factor(treat)+factor(study)+(treat-1|study),family =binomial,data=X$data)
   
-  BN2=glmer(cbind(events,n-events)~factor(treat)+(treat-1|study)+(1|study),family =binomial,data=X$data)
   
   BN3=glm(cbind(events,n-events)~factor(treat)+factor(study),family =binomial,data=X$data)
   
-  ests=summary(BN1)$coefficients
-  ests=ests[grep("treat",rownames(ests)),]
-  ests=ests[,-c(3,4)]
-  #ests[,2]=ests[,2]*sqrt(phi)
-  ci.lower=ests[,1]-1.96*ests[,2]
-  ci.upper=ests[,1]+1.96*ests[,2]
-  BN.res=data.frame(mean=ests[,1],lowerCI=ci.lower,upperCI=ci.upper)
-  biasBN<-c(biasBN, (BN.res$mean-X$logOR))
-  BN.res$cover<-(BN.res$lowerCI<X$logOR)&(BN.res$upperCI>X$logOR)
-  coverageBN<-c(coverageBN, BN.res$cover)
-  mseBN=c(mseBN,biasBN*biasBN+ests[,2]*ests[,2])
-  
-  se_BN=c(se_BN,ests[,2])
-  
-  length_BN=c(length_BN,ci.upper-ci.lower)
-  
-  heterogeneity=data.frame(VarCorr(BN1))[,4]
-  counterBN=(heterogeneity>0)
-  
-  lower_BN=c(lower_BN,BN.res$lowerCI)
-  upper_BN=c(upper_BN,BN.res$upperCI)
-  estimate_BN=c(estimate_BN,BN.res$mean)
-  
-  
-  m_fixed <- tryCatch({
-    withCallingHandlers({
-      error <- FALSE
-      list(model = glmer(cbind(events,n-events)~factor(treat)+factor(study)+(treat-1|study),family =binomial,data=X$data),
-           error = error)
-    },warning = function(w) {
-      if(grepl('failed to converge', w$message)) error <<- TRUE
-    }
-    )})
-  
-  count_fail_fixed=c(count_fail_fixed,m_fixed$error)
-  
-  
-  
-  
-  
-  #####################################################################
-  
-  
-  ests_random=summary(BN2)$coefficients
-  ests_random=ests_random[grep("treat",rownames(ests_random)),]
-  ests_random=ests_random[,-c(3,4)]
-  #ests[,2]=ests[,2]*sqrt(phi)
-  ci.lower_random=ests_random[,1]-1.96*ests_random[,2]
-  ci.upper_random=ests_random[,1]+1.96*ests_random[,2]
-  
-  BN.res_random=data.frame(mean=ests_random[,1],lowerCI=ci.lower_random,upperCI=ci.upper_random)
-  
-  biasBN_random<-c(biasBN_random, (BN.res_random$mean-X$logOR))
-  BN.res_random$cover<-(BN.res_random$lowerCI<X$logOR)&(BN.res_random$upperCI>X$logOR)
-  coverageBN_random<-c(coverageBN_random, BN.res_random$cover)
-  mseBN_random=c(mseBN_random,biasBN_random*biasBN_random+ests_random[,2]*ests_random[,2])
-  se_BN_random=c(se_BN_random,ests_random[,2])
-  length_BN_random=c(length_BN_random,ci.upper_random-ci.lower_random)
-  
-  heterogeneity_random=data.frame(VarCorr(BN2))[,4]
-  counterBN_random=(heterogeneity>0)
-  
-  lower_BN_random=c(lower_BN_random,BN.res_random$lowerCI)
-  upper_BN_random=c(upper_BN_random,BN.res_random$upperCI)
-  estimate_BN_random=c(estimate_BN_random,BN.res_random$mean)
-  
-  heterogeneity_random=VarCorr(BN2)
-  C=as.data.frame(heterogeneity_random)
-  
-  C=subset(C,C$var1=="treat")
-  heterogeneity_random=C$sdcor
-  
-  
-  m_random <- tryCatch({
-    withCallingHandlers({
-      error <- FALSE
-      list(model = glmer(cbind(events,n-events)~factor(treat)+(treat-1|study)+(1|study),family =binomial,data=X$data),
-           error = error)
-    },warning = function(w) {
-      if(grepl('failed to converge', w$message)) error <<- TRUE
-    }
-    )})
-  
-  count_fail_random=c(count_fail_random,m_random$error)
-  
-  
-  
-  #####################################################################
+ 
+   #####################################################################
   
   
   ests_glm=summary(BN3)$coefficients
@@ -734,62 +629,28 @@ BN=function(X)
   
   #####################################################################
   
-  return(list("bias"=biasBN,"cov"=coverageBN,"mseBN"=mseBN,"counterBN"=counterBN,"heterogeneity"=heterogeneity,
-              "se_BN"=se_BN,"length_BN"=length_BN,
-              "count_fail_fixed"=count_fail_fixed,
-              "bias_random"=biasBN_random,"cov_random"=coverageBN_random,"mseBN_random"=mseBN_random,
-              "counterBN_random"=counterBN_random,"heterogeneity_random"=heterogeneity_random,
-              "se_BN_random"=se_BN_random,
-              "length_BN_random"=length_BN_random,
-              "count_fail_random"=count_fail_random,
-              "bias_glm"=biasBN_glm,"cov_glm"=coverageBN_glm,"mse_glm"=mseBN_glm,
+  return(list("bias_glm"=biasBN_glm,"cov_glm"=coverageBN_glm,"mse_glm"=mseBN_glm,
               "se_glm"=se_BN_glm,"length_glm"=length_BN_glm,
               "lower_BN"=lower_BN,"upper_BN"=upper_BN,"estimate_BN"=estimate_BN,
               "lower_BN_random"=lower_BN_random,"upper_BN_random"=upper_BN_random,"estimate_BN_random"=estimate_BN_random,
-              "lower_glm"=lower_glm,"upper_glm"=upper_glm,"estimate_glm"=estimate_glm
-              
-  )
-  )
-  
-}
+              "lower_glm"=lower_glm,"upper_glm"=upper_glm,"estimate_glm"=estimate_glm)
 
+  )  
+  }
+            
 clusterExport(cl2,"X3")
 clusterExport(cl2,"NT")
 clusterExport(cl2,"N.sim")
 clusterExport(cl2, "BN")
-clusterEvalQ(cl2, {library(lme4)})
+clusterEvalQ(cl2, {library(glm)})
 l9=parLapply(cl2,1:N.sim, function(x) BN(X3[[x]]))
 
-biasBN=c()
-for (i in 1:N.sim){  biasBN=c(biasBN, l9[[i]]$bias)}
-
-biasBN_random=c()
-for (i in 1:N.sim){  biasBN_random=c(biasBN_random, l9[[i]]$bias_random)}
 
 bias_glm=c()
 for (i in 1:N.sim){  bias_glm=c(bias_glm, l9[[i]]$bias_glm)}
 
-
-mean(biasBN)
-mean(biasBN_random)
 mean(bias_glm)
-
-se_BN=c()
-for (i in 1:N.sim){  se_BN=c(se_BN, l9[[i]]$se_BN)}
-
-length_BN=c()
-for (i in 1:N.sim){  length_BN=c(length_BN, l9[[i]]$length_BN)}
-mean(length_BN)
-
-
-se_BN_random=c()
-for (i in 1:N.sim){  se_BN_random=c(se_BN_random, l9[[i]]$se_BN_random)}
-
-length_BN_random=c()
-for (i in 1:N.sim){  length_BN_random=c(length_BN_random, l9[[i]]$length_BN_random)}
-mean(length_BN_random)
-
-
+             
 se_glm=c()
 for (i in 1:N.sim){  se_glm=c(se_glm, l9[[i]]$se_glm)}
 
@@ -797,72 +658,14 @@ length_glm=c()
 for (i in 1:N.sim){  length_glm=c(length_glm, l9[[i]]$length_glm)}
 mean(length_glm)
 
-
-coverageBN=c()
-for (i in 1:N.sim){  coverageBN=c(coverageBN, l9[[i]]$cov)}
-mean(coverageBN)
-
-
-coverageBN_random=c()
-for (i in 1:N.sim){  coverageBN_random=c(coverageBN_random, l9[[i]]$cov_random)}
-mean(coverageBN_random)
-
 coverage_glm=c()
 for (i in 1:N.sim){  coverage_glm=c(coverage_glm, l9[[i]]$cov_glm)}
 mean(coverage_glm)
-
-mseBN=c()
-for (i in 1:N.sim){  mseBN=c(mseBN, l9[[i]]$mseBN)}
-mean(mseBN)
-
-
-mseBN_random=c()
-for (i in 1:N.sim){  mseBN_random=c(mseBN_random, l9[[i]]$mseBN_random)}
-mean(mseBN_random)
-
 
 mse_glm=c()
 for (i in 1:N.sim){  mse_glm=c(mse_glm, l9[[i]]$mse_glm)}
 mean(mse_glm)
 
-
-counterBN=c()
-for (i in 1:N.sim){counterBN=c(counterBN, l9[[i]]$counterBN)}
-sum(counterBN)
-
-
-counterBN_random=c()
-for (i in 1:N.sim){counterBN_random=c(counterBN_random, l9[[i]]$counterBN_random)}
-sum(counterBN_random)
-
-
-heterogeneity=c()
-for (i in 1:N.sim){heterogeneity=c(heterogeneity, l9[[i]]$heterogeneity)}
-tau2_BN=heterogeneity
-
-
-heterogeneity_random=c()
-for (i in 1:N.sim){heterogeneity_random=c(heterogeneity_random, l9[[i]]$heterogeneity_random)}
-tau2_BN_random=heterogeneity_random
-
-
-lower_BN=c()
-for (i in 1:N.sim){lower_BN=c(lower_BN, l9[[i]]$lower_BN)}
-
-upper_BN=c()
-for (i in 1:N.sim){upper_BN=c(upper_BN, l9[[i]]$upper_BN)}
-
-estimate_BN=c()
-for (i in 1:N.sim){estimate_BN=c(estimate_BN, l9[[i]]$estimate_BN)}
-
-lower_BN_random=c()
-for (i in 1:N.sim){lower_BN_random=c(lower_BN_random, l9[[i]]$lower_BN_random)}
-
-upper_BN_random=c()
-for (i in 1:N.sim){upper_BN_random=c(upper_BN_random, l9[[i]]$upper_BN_random)}
-
-estimate_BN_random=c()
-for (i in 1:N.sim){estimate_BN_random=c(estimate_BN_random, l9[[i]]$estimate_BN_random)}
 
 lower_glm=c()
 for (i in 1:N.sim){lower_glm=c(lower_glm, l9[[i]]$lower_glm)}
@@ -873,78 +676,10 @@ for (i in 1:N.sim){upper_glm=c(upper_glm, l9[[i]]$upper_glm)}
 estimate_glm=c()
 for (i in 1:N.sim){estimate_glm=c(estimate_glm, l9[[i]]$estimate_glm)}
 
-count_fail_fixed=c()
-for (i in 1:N.sim){count_fail_fixed=c(count_fail_fixed, l9[[i]]$count_fail_fixed)}
-
-
-count_fail_random=c()
-for (i in 1:N.sim){count_fail_random=c(count_fail_random, l9[[i]]$count_fail_random)}
-
-count_fail=cbind.data.frame(count_fail_fixed,count_fail_random)
-
-summaries_fail=data.frame(sum(count_fail[,1]),sum(count_fail[,2]))
-names(summaries_fail)=c("fail fixed intercept","fail random intercept")
-
-new_het=cbind.data.frame(heterogeneity,heterogeneity_random)
-
-bias_taulme4_fixed=new_het[,1]-tau
-bias_taulme4_random=new_het[,2]-tau
-
-bias_taulme4=cbind.data.frame(bias_taulme4_fixed,bias_taulme4_random)
-names(bias_taulme4)=c("fixed intercept","random intercept")
-
-bias_taulme4_mean=cbind.data.frame(mean(bias_taulme4[,1]),mean(bias_taulme4[,2]))
-names(bias_taulme4_mean)=c("fixed intercept","random intercept")
 
 #############################
 
 ################################### RESULTS ##################################################
-
-
-round(mean(biasIV.FE), digits=2)
-round(mean(abs(biasIV.FE)), digits=2)
-round(mean(coverageIV.FE), digits=3)*100
-round(mean(mseIV.FE),digits = 2)
-# IV.RE
-round(mean(biasIV.RE), digits=2)
-round(mean(abs(biasIV.RE)), digits=2)
-round(mean(coverageIV.RE), digits=3)*100
-round(mean(mseIV.RE),digits = 2)
-
-avg_bias_tau_IV=c(round(mean(bias_tau_IV), digits=2))
-avg_bias_tau_IV=data.frame(avg_bias_tau_IV)
-names(avg_bias_tau_IV)=c("mean_bias_tau")
-# MH
-round(mean(biasMH), digits=2)
-round(mean(abs(biasMH)), digits=2)
-round(mean(coverageMH), digits=3)*100
-round(mean(mseMH),digits = 2)
-# NCH
-round(mean(biasNCH), digits=2)
-round(mean(abs(biasNCH)), digits=2)
-round(mean(coverageNCH), digits=3)*100
-round(mean(mseNCH),digits = 2)
-# FL_F
-round(mean(biasFL_F), digits=2)
-round(mean(abs(biasFL_F)), digits=2)
-round(mean(coverageFL_F), digits=3)*100
-round(mean(coverageFL_Fr), digits=3)*100
-round(mean(pcoverageFL_F), digits=3)*100
-round(mean(mseFL_F),digits = 2)
-round(mean(mseFL_Fr),digits = 2)
-
-#BN
-round(mean(biasBN), digits=2)
-round(mean(abs(biasBN)), digits=2)
-round(mean(coverageBN), digits=3)*100
-round(mean(mseBN),digits = 2)
-## BN random
-
-
-round(mean(biasBN_random), digits=2)
-round(mean(abs(biasBN_random)), digits=2)
-round(mean(coverageBN_random), digits=3)*100
-round(mean(mseBN_random),digits = 2)
 
 
 
@@ -953,68 +688,63 @@ round(mean(mseBN_random),digits = 2)
 ############
 
 bias=c(round(mean(biasIV.FE), digits=2),round(mean(biasIV.RE), digits=2),round(mean(biasMH), digits=2),round(mean(biasNCH), digits=2),
-       round(mean(biasFL_F), digits=2),round(mean(biasFL_F), digits=2),round(mean(biasBN), digits=2),
-       round(mean(biasBN_random), digits=2),
-       round(mean(bias_glm), digits=2)
+       round(mean(biasFL_F), digits=2),round(mean(biasFL_F), digits=2),round(mean(bias_glm), digits=2))
+  
        
-)
+       
+
 
 
 
 
 
 absolute_bias=c(round(mean(abs(biasIV.FE)), digits=2),round(mean(abs(biasIV.RE)), digits=2),round(mean(abs(biasMH)), digits=2),
-                round(mean(abs(biasNCH)), digits=2),round(mean(abs(biasFL_F)), digits=2),round(mean(abs(biasFL_F)), digits=2)
-                ,round(mean(abs(biasBN)), digits=2),
-                round(mean(abs(biasBN_random)), digits=2),
-                round(mean(abs(bias_glm)), digits=2)
-                
-)
-
+                round(mean(abs(biasNCH)), digits=2),round(mean(abs(biasFL_F)), digits=2),round(mean(abs(biasFL_F)), digits=2), round(mean(abs(bias_glm)), digits=2)
+                )
+             
 coverage=c(round(mean(coverageIV.FE), digits=3)*100,round(mean(coverageIV.RE), digits=3)*100,round(mean(coverageMH), digits=3)*100,
            round(mean(coverageNCH), digits=3)*100,round(mean(coverageFL_F), digits=3)*100,round(mean(coverageFL_Fr), digits=3)*100,
-           round(mean(coverageBN), digits=3)*100,
-           round(mean(coverageBN_random), digits=3)*100,
            round(mean(coverage_glm), digits=3)*100
 )
-
-coverage1=c(round(mean(coverageIV.FE), digits=3)*100,round(mean(coverageIV.RE), digits=3)*100,round(mean(coverageMH), digits=3)*100,
+  
+ coverage1=c(round(mean(coverageIV.FE), digits=3)*100,round(mean(coverageIV.RE), digits=3)*100,round(mean(coverageMH), digits=3)*100,
             round(mean(coverageNCH), digits=3)*100,round(mean(pcoverageFL_F), digits=3)*100,round(mean(coverageFL_Fr), digits=3)*100,
-            round(mean(coverageBN), digits=3)*100,
-            
-            round(mean(coverageBN_random), digits=3)*100,
-            round(mean(coverage_glm), digits=3)*100
-)
+            round(mean(coverage_glm), digits=3)*100)
+
 
 mse=c(round(mean(mseIV.FE),digits = 2),round(mean(mseIV.RE),digits = 2),round(mean(mseMH),digits = 2),round(mean(mseNCH),digits = 2),
-      round(mean(mseFL_F),digits = 2),round(mean(mseFL_Fr),digits = 2),round(mean(mseBN),digits = 2),
-      round(mean(mseBN_random),digits = 2),round(mean(mse_glm),digits = 2)
+      round(mean(mseFL_F),digits = 2),round(mean(mseFL_Fr),digits = 2),round(mean(mse_glm),digits = 2))
       
-)
 
 results=cbind(bias,absolute_bias,mse,coverage)
 results=data.frame(results)
-row.names(results)=c("IV_FE","IV_RE","MH","NCH","FL_F","FL_F(R)","B-N","B-N random intercept","Common logistic")
-results
+row.names(results)=c("IV_FE","IV_RE","MH","NCH","FL_F","FL_F(R)","Common logistic")
+results               
 
 results1=cbind(bias,absolute_bias,mse,coverage1)
 results1=data.frame(results1)
-row.names(results1)=c("IV_FE","IV_RE","MH","NCH","FL_F(P)","FL_F(R)","B-N","B-N random intercept","Common logistic")
+row.names(results1)=c("IV_FE","IV_RE","MH","NCH","FL_F(P)","FL_F(R)","Common logistic")
 results1
-
 
 het1=cbind(counter,dispersion)
 het1=as.data.frame(het1)
 names(het1)=c("counter_Fletcher","dispersion")
-het2=cbind(counterBN,tau2_BN,counterBN_random,tau2_BN_random)
-het2=as.data.frame(het2)
 
-het=data.frame(het1,het2)
 
 
 tau_info_IV=data.frame(bias_tau_IV,tau_IV)
 names(tau_info_IV)=c("bias_tau_IV","tau_IV")
 
 countIV=ifelse(tau_IV>0,1,0)
+
+
+
+
+
+
+
+
+
+
 
 
